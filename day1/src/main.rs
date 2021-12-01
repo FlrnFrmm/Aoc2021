90a404cb392347d76cdf1859a984b095b72918c1
Florian Fromm <flrn.frmm@gmail.com>
2021-12-01 10:58:36 +0100

    Improve day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,7 @@
 fn main() -> anyhow::Result<()> {
     let values = utility::read_input(None)?;
-
     println!("Part One: {}", part_one(&values));
-
     println!("Part Two: {}", part_two(&values));
-
     Ok(())
 }
 
@@ -12,7 +9,7 @@ fn part_one(values: &Vec<i32>) -> usize {
     values
         .iter()
         .skip(1)
-        .fold((0usize, 0i32), |(mut count, last), v| {
+        .fold((0usize, values[0]), |(mut count, last), v| {
             if *v > last {
                 count += 1;
             }
@@ -22,14 +19,9 @@ fn part_one(values: &Vec<i32>) -> usize {
 }
 
 fn part_two(values: &Vec<i32>) -> usize {
-    let mut last = values[..3].iter().sum::<i32>();
-    let mut count = 0;
-    for i in 1..values.len() - 2 {
-        let next = values[i..i + 3].iter().sum::<i32>();
-        if next > last {
-            count += 1;
-        }
-        last = next;
-    }
-    count
+    let values = values
+        .windows(3)
+        .map(|vs| vs.iter().sum::<i32>())
+        .collect::<Vec<i32>>();
+    part_one(&values)
 }
