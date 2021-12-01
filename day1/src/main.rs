e13ff2b4d79d731d8456f665b5253a720db72388
Florian Fromm <flrn.frmm@gmail.com>
2021-12-01 10:42:57 +0100

    Add day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let values = utility::read_input(None)?;
+
+    println!("Part One: {}", part_one(&values));
+
+    println!("Part Two: {}", part_two(&values));
+
+    Ok(())
+}
+
+fn part_one(values: &Vec<i32>) -> usize {
+    values
+        .iter()
+        .skip(1)
+        .fold((0usize, 0i32), |(mut count, last), v| {
+            if *v > last {
+                count += 1;
+            }
+            (count, *v)
+        })
+        .0
+}
+
+fn part_two(values: &Vec<i32>) -> usize {
+    let mut last = values[..3].iter().sum::<i32>();
+    let mut count = 0;
+    for i in 1..values.len() - 2 {
+        let next = values[i..i + 3].iter().sum::<i32>();
+        if next > last {
+            count += 1;
+        }
+        last = next;
+    }
+    count
 }
--- utility/src/lib.rs
+++ utility/src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use std::fs;
+
+pub fn read_input(path: Option<&str>) -> anyhow::Result<Vec<i32>> {
+    let path = path.unwrap_or("input/input.txt");
+    let input = fs::read_to_string(path)?;
+    let values: Vec<i32> = input.lines().map(|v| v.parse::<i32>().unwrap()).collect();
+    Ok(values)
 }
